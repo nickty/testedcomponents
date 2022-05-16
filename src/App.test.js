6be0e0404d4afe4574fe074afcf346fe38c3af60
Mizanur Rahman <mizanur.rahman@navana.com>
2022-05-16 22:32:21 +0600

    commented everything test

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
+// testing by text inside the document
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
+// testing by finding list time by finding with role
 test('renders 3 list items', () => {
   render(<App />);
   const listItems = screen.getAllByRole("listitem");
   expect(listItems).toHaveLength(4)
 });
 
+// testing by id
 test('test the title', () => {
   render(<App />);
   const listItems = screen.getByTestId("mytestid");
   expect(listItems).toBeInTheDocument()
 });
 
+// Test by getting after title
 test('test the sum', () => {
   render(<App />);
   const listItems = screen.getByTitle("sum");
